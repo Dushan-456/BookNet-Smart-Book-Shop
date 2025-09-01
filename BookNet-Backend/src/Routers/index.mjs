57686af0bd5ff2819a43605db0321bea0d359c25
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-01 16:13:16 +0530

    Update index.mjs

--- BookNet-Backend/src/Routers/index.mjs
+++ BookNet-Backend/src/Routers/index.mjs
@@ -1,12 +1,12 @@
 import { Router } from "express";
-// import userRouter from "./user.mjs";
+import userRouter from "./userRouter.mjs";
 
 
 const rootRouter = Router();
 
 rootRouter.get("/",(req,res)=> res.sendStatus(200));
 
-// rootRouter.use("/user",userRouter)
+rootRouter.use("/user",userRouter)
 
 
 
