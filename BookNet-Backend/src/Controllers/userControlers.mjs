3279b71eec83ae08c1c17f35d7bacbdf09b4996b
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-04 00:01:08 +0530

    Update userControlers.mjs

--- BookNet-Backend/src/Controllers/userControlers.mjs
+++ BookNet-Backend/src/Controllers/userControlers.mjs
@@ -2,6 +2,7 @@ import { errorCreate } from "../Utils/error-creator.mjs";
 import { matchedData, validationResult } from "express-validator";
 import DB from "../db/db.mjs";
 import bcrypt from "bcrypt";
+import { generateToken } from "../Utils/jwt.mjs";
 
 class UserController {
    // Create new user------------------------------------------------------------------------------------------------------------------------------
@@ -19,11 +20,11 @@ class UserController {
       const { firstName, lastName, username, email, password } =
          matchedData(req);
       try {
-         // 3. Hash the password
+         // Hash the password
          const salt = await bcrypt.genSalt(10);
          const hashedPassword = await bcrypt.hash(password, salt);
 
-         // 4. Create a new user in the database
+         // Create a new user in the database
          const newUser = await DB.user.create({
             data: {
                firstName,
@@ -34,23 +35,77 @@ class UserController {
             },
          });
 
-         // 5. Respond with the created user (omitting the password)
+         // Respond with the created user (omitting the password)
          const { passwordHash: _, ...userWithoutPassword } = newUser;
+         const token = generateToken({ userId: newUser.id });
+
          res.status(201).json({
             message: "User created successfully!",
             user: userWithoutPassword,
+            token,
          });
       } catch (error) {
          console.error("Error Registering user:", error);
 
          if (error.code === "P2002") {
-            const field = error.meta.target[0]; // e.g., 'Username' or 'Email'
+            const field = error.meta.target[0]; //  'Username' or 'Email'
             return res
                .status(409)
                .json({ message: `${field} is already taken.` });
          }
 
-         res.status(500).json({ message: "An unexpected error occurred." });
+         res.status(500).json({ message: "Server error during registration" });
+      }
+   };
+
+   // User Login------------------------------------------------------------------------------------------------------------------------------
+
+   loginUser = async (req, res) => {
+      const error = validationResult(req);
+      const creatingError = errorCreate(error.array());
+      if (error.array().length) {
+         return res.status(400).json({
+            msg: "Valiation error",
+            error: creatingError,
+            data: null,
+         });
+      }
+
+      const { emailOrUsername, password } = matchedData(req);
+
+      try {
+         // Find user by email OR username
+         const user = await DB.user.findFirst({
+            where: {
+               OR: [
+                  { email: emailOrUsername }, 
+                  { username: emailOrUsername }, 
+               ],
+            },
+         });
+
+         if (!user) {
+            return res.status(401).json({ message: "User Name or Email Not Registered in Sytem" });
+         }
+
+         //  Compare passwords
+         const isMatch = await bcrypt.compare(password, user.passwordHash);
+
+         if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+         }
+
+         //  Generate token and respond
+         const token = generateToken({ userId: user.id });
+         res.status(200).json({
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            token,
+         });
+      } catch (error) {
+         console.error(error);
+         res.status(500).json({ message: "Server error during login" });
       }
    };
 
