5bf76e7c86a5861e9d166d50dbbfd2a6d91266d8
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-03 23:13:37 +0530

    Delete migration.sql

--- BookNet-Backend/prisma/migrations/20250903104154_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Profile" ADD COLUMN     "Address" TEXT,
-ADD COLUMN     "DOB" TIMESTAMP(3),
-ADD COLUMN     "Designation" TEXT,
-ADD COLUMN     "FirstName" TEXT,
-ADD COLUMN     "Gender" TEXT,
-ADD COLUMN     "LastName" TEXT,
-ADD COLUMN     "Mobile" TEXT,
-ALTER COLUMN "Image" DROP NOT NULL;
