e28df30cb06ba1d4fcc7a242b28f190956f93dac
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-03 23:14:29 +0530

    Update userControlers.mjs

--- BookNet-Backend/src/Controllers/userControlers.mjs
+++ BookNet-Backend/src/Controllers/userControlers.mjs
@@ -1,162 +1,159 @@
 import { errorCreate } from "../Utils/error-creator.mjs";
 import { matchedData, validationResult } from "express-validator";
 import DB from "../db/db.mjs";
-import bcrypt from 'bcrypt';
-
-
+import bcrypt from "bcrypt";
 
 class UserController {
-    // Create new user------------------------------------------------------------------------------------------------------------------------------
-    RegisterNewUser = async (req, res) => {
+   // Create new user------------------------------------------------------------------------------------------------------------------------------
+   RegisterNewUser = async (req, res) => {
+      const error = validationResult(req);
+      const creatingError = errorCreate(error.array());
+      if (error.array().length) {
+         return res.status(400).json({
+            msg: "Valiation error",
+            error: creatingError,
+            data: null,
+         });
+      }
 
-        const error = validationResult(req);
-        const creatingError = errorCreate(error.array());
-        if (error.array().length) {
-            return res.status(400).json({
-                msg: "error",
-                error: creatingError,
-                data: null,
-            });
-        }
+      const { firstName, lastName, username, email, password } =
+         matchedData(req);
+      try {
+         // 3. Hash the password
+         const salt = await bcrypt.genSalt(10);
+         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const { FirstName, LastName, Username, Email, Password } = matchedData(req);
-        try {
-            // 3. Hash the password
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(Password, salt);
+         // 4. Create a new user in the database
+         const newUser = await DB.user.create({
+            data: {
+               firstName,
+               lastName,
+               username,
+               email,
+               passwordHash: hashedPassword,
+            },
+         });
 
-            // 4. Create a new user in the database
-            const newUser = await DB.user.create({
-                data: {
-                    FirstName,
-                    LastName,
-                    Username,
-                    Email,
-                    Password: hashedPassword,
-                },
-            });
+         // 5. Respond with the created user (omitting the password)
+         const { passwordHash: _, ...userWithoutPassword } = newUser;
+         res.status(201).json({
+            message: "User created successfully!",
+            user: userWithoutPassword,
+         });
+      } catch (error) {
+         console.error("Error Registering user:", error);
 
-            // 5. Respond with the created user (omitting the password)
-            const { Password: _, ...userWithoutPassword } = newUser;
-            res.status(201).json({
-                message: "User created successfully!",
-                user: userWithoutPassword
-            });
+         if (error.code === "P2002") {
+            const field = error.meta.target[0]; // e.g., 'Username' or 'Email'
+            return res
+               .status(409)
+               .json({ message: `${field} is already taken.` });
+         }
 
-        } catch (error) {
-            console.error('Error Registering user:', error);
+         res.status(500).json({ message: "An unexpected error occurred." });
+      }
+   };
 
-            if (error.code === 'P2002') {
-                const field = error.meta.target[0]; // e.g., 'Username' or 'Email'
-                return res.status(409).json({ message: `${field} is already taken.` });
-            }
+   //Get All Users------------------------------------------------------------------------------------------------------------------------------
+   showAllUsers = async (req, res) => {
+      try {
+         const users = await DB.user.findMany({
+            select: {
+               id: true,
+               username: true,
+               firstName: true,
+               lastName: true,
+               email: true,
+               role: true,
+               createdAt: true,
+               updatedAt: true,
 
-            res.status(500).json({ message: 'An unexpected error occurred.' });
-        }
+               Profile: {
+                  select: {
+                     image: true,
+                  },
+               },
+            },
+         });
+         return res.status(200).json({
+            msg: "All Users",
+            data: users,
+         });
+      } catch (error) {
+         console.error("Error :", error);
 
+         return res.status(500).json({
+            msg: "error",
+            error: "Internal Server Error",
+         });
+      }
+   };
 
+   //DeleteUser by ID------------------------------------------------------------------------------------------------------------------------------
+   deleteUserById = async (req, res) => {
+      const { id } = req.params;
 
-    }
+      if (!id) {
+         return res.status(400).json({ message: "User ID is required." });
+      }
 
-    //Get All Users------------------------------------------------------------------------------------------------------------------------------
-    showAllUsers = async (req, res) => {
-        try {
-            const users = await DB.user.findMany({
-                select: {
-                    id: true,
-                    Username: true,
-                    FirstName: true,
-                    LastName: true,
-                    Email: true,
-                    createAt: true,
-                    updateAt: true,
+      try {
+         await DB.user.delete({
+            where: {
+               id,
+            },
+         });
 
-                    Profile: {
-                        select: {
-                            Image: true,
-                        },
-                    },
-                },
-            });
-            return res.status(200).json({
-                msg: "All Users",
-                data: users,
-            });
-        } catch (error) {
-            console.error('Error :', error);
+         return res.status(200).json({ message: "User deleted successfully." });
+      } catch (error) {
+         console.error("Error deleting user:", error);
+         if (error.code === "P2025") {
+            return res
+               .status(404)
+               .json({ message: `User with ID ${id} not found.` });
+         }
+         return res.status(500).json({
+            msg: "error",
+            error: "Internal Server Error",
+         });
+      }
+   };
 
-            return res.status(500).json({
-                msg: "error",
-                error: "Internal Server Error",
-            });
-        }
-    };
+   //Get User Profile Details by ID------------------------------------------------------------------------------------------------------------------------------
 
-    //DeleteUser by ID------------------------------------------------------------------------------------------------------------------------------
-    deleteUserById = async (req, res) => {
-        const { id } = req.params;
-        const userId = parseInt(id);
+   getUserAndProfileById = async (req, res) => {
+      const { id } = req.params;
+      if (!id) {
+         return res.status(400).json({ message: "User ID is required." });
+      }
+      try {
+         const user = await DB.user.findUnique({
+            where: {
+               id,
+            },
+            select: {
+               id: true,
+               username: true,
+               email: true,
+               Profile: true,
+            },
+         });
 
-        if (isNaN(userId)) {
-            return res.status(400).json({ message: 'Invalid user ID provided.' });
-        }
+         if (!user) {
+            return res
+               .status(404)
+               .json({ message: `User with ID ${id} not found.` });
+         }
 
-        try {
-            const users = await DB.user.delete({
-                where: {
-                    id: userId,
-                },
-            });
-
-            return res.status(200).json({ message: 'User deleted successfully.' });
-
-        } catch (error) {
-
-            console.error('Error deleting user:', error);
-            return res.status(500).json({
-                msg: "error",
-                error: "Internal Server Error",
-            });
-        }
-    };
-
-    //Get User Profile Details by ID------------------------------------------------------------------------------------------------------------------------------
-
-    getUserAndProfileById = async (req, res) => {
-        const { id } = req.params;
-        const userId = parseInt(id);
-
-        if (isNaN(userId)) {
-            return res.status(400).json({ message: 'Invalid User ID provided.' });
-        }
-
-        try {
-            const user = await DB.user.findUnique({
-                where: {
-                    id: userId,
-                },
-                select: {
-                    id: true,
-                    Username: true,
-                    Email: true,
-                    Profile: true,
-                },
-            });
-
-            if (!user) {
-                return res.status(404).json({ message: `User with ID ${userId} not found.` });
-            }
-
-            res.status(200).json({
-                message: "User and profile retrieved successfully!",
-                data: user,
-            });
-
-        } catch (error) {
-            console.error("Error fetching user and profile:", error);
-            res.status(500).json({ message: "An unexpected error occurred." });
-        }
-    };
+         res.status(200).json({
+            message: "User and profile retrieved successfully!",
+            data: user,
+         });
+      } catch (error) {
+         console.error("Error fetching user and profile:", error);
+         res.status(500).json({ message: "An unexpected error occurred." });
+      }
+   };
 }
 
 export default new UserController();
