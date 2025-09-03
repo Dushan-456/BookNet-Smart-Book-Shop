923e56b3d57cc226213282c4cb5a445c44b665ec
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-03 23:13:39 +0530

    Delete migration.sql

--- BookNet-Backend/prisma/migrations/20250903105446_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."Profile" DROP CONSTRAINT "Profile_UserId_fkey";
-
--- AddForeignKey
-ALTER TABLE "public"."Profile" ADD CONSTRAINT "Profile_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
