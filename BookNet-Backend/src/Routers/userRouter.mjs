6f828f317c8a15d69e5849a7fc8fff6f583fa54b
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-03 17:41:13 +0530

    Update userRouter.mjs

--- BookNet-Backend/src/Routers/userRouter.mjs
+++ BookNet-Backend/src/Routers/userRouter.mjs
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import userControlers from "../Controllers/userControlers.mjs";
-import {  RegisterValidator, userDataValidator } from "../Utils/validationMethods.mjs";
+import {  ProfileFieldsValidator, RegisterValidator } from "../Utils/validationMethods.mjs";
+import profileControlers from "../Controllers/profileControlers.mjs";
 
 const userRouter = Router();
 
 userRouter.post("/register", RegisterValidator(), userControlers.RegisterNewUser);
 
-userRouter.delete("/delete-user/:id", userControlers.deleteUserById);
+userRouter.delete("/delete/:id", userControlers.deleteUserById);
 
 userRouter.get("/all-users", userControlers.showAllUsers);
 
-// userRouter.get("/:id", userControlers.getUserById);
+userRouter.post("/profile/:id",ProfileFieldsValidator(), profileControlers.createOrUpdateUserProfile);
+
+userRouter.get("/:id",userControlers.getUserAndProfileById);
 
 export default userRouter;
