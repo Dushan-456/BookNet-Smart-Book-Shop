33a37acbbb8304f5fa3289e063e4e3fd4d94d110
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-05 00:18:02 +0530

    Create productRouter.mjs

---
+++ BookNet-Backend/src/Routers/productRouter.mjs
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import {
+   productFieldsValidator,
+} from "../middleware/validationMethods.mjs";
+import { loginProtect, protectedToAdmin } from "../middleware/authMiddleware.mjs";
+import productController from "../Controllers/productController.mjs";
+
+const productRouter = Router();
+
+// --- PUBLIC ROUTES ---
+
+productRouter.get("/", productController.getAllProducts);
+productRouter.get("/:id", productController.getProductById);
+
+
+
+// // --- ADMIN-ONLY ROUTES (Require Authentication & Authorization) ---
+
+productRouter.post("/", loginProtect,protectedToAdmin,productFieldsValidator(), productController.createProduct);
+productRouter.put("/:id", loginProtect,protectedToAdmin,productFieldsValidator(), productController.updateProduct);
+productRouter.delete("/:id", loginProtect,protectedToAdmin,productFieldsValidator(), productController.deleteProduct);
+
+export default productRouter;
