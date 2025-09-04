786df24168b19c6b94cf10a4eea17b8819da2cba
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-04 23:00:26 +0530

    Create categoryRouter.mjs

---
+++ BookNet-Backend/src/Routers/categoryRouter.mjs
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import categoryController from "../Controllers/categoryController.mjs";
+import { loginProtect, protectedToAdmin } from "../middleware/authMiddleware.mjs";
+import { CategoryFieldsValidator } from "../middleware/validationMethods.mjs";
+
+const categoryRouter = Router();
+
+// --- PUBLIC ROUTES ---
+
+categoryRouter.get("/", categoryController.getAllCategories);
+
+
+// // --- ADMIN-ONLY ROUTES (Require Authentication & Authorization) ---
+
+categoryRouter.post("/", loginProtect,protectedToAdmin,CategoryFieldsValidator(),categoryController.createCategory);
+categoryRouter.put("/:id", loginProtect,protectedToAdmin,CategoryFieldsValidator(),categoryController.updateCategory);
+categoryRouter.delete("/:id", loginProtect,protectedToAdmin,categoryController.deleteCategory);
+
+export default categoryRouter;
