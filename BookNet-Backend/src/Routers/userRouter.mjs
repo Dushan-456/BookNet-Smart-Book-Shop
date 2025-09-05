ebcd432276427e745287d5f7460a0d398cb0125e
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-05 14:46:47 +0530

    Update userRouter.mjs

--- BookNet-Backend/src/Routers/userRouter.mjs
+++ BookNet-Backend/src/Routers/userRouter.mjs
@@ -1,30 +1,30 @@
 import { Router } from "express";
-import userControlers from "../Controllers/userControlers.mjs";
-import profileControlers from "../Controllers/profileControlers.mjs";
 import {
    loginValidator,
    ProfileFieldsValidator,
    RegisterValidator,
 } from "../middleware/validationMethods.mjs";
 import { loginProtect, protectedToAdmin } from "../middleware/authMiddleware.mjs";
+import profileControllers from "../Controllers/profileControllers.mjs";
+import userControllers from "../Controllers/userControllers.mjs";
 
 const userRouter = Router();
 
 // --- PUBLIC ROUTES ---
 
-userRouter.post("/register",RegisterValidator(), userControlers.RegisterNewUser);
-userRouter.post("/login", loginValidator(), userControlers.loginUser);
+userRouter.post("/register",RegisterValidator(), userControllers.RegisterNewUser);
+userRouter.post("/login", loginValidator(), userControllers.loginUser);
 
 
 // --- PROTECTED ROUTES (Require Authentication) ---
 
-userRouter.post("/:id",ProfileFieldsValidator(),loginProtect,profileControlers.createOrUpdateUserProfile);
-userRouter.get("/:id", loginProtect, userControlers.getUserAndProfileById);
+userRouter.post("/:id",ProfileFieldsValidator(),loginProtect,profileControllers.createOrUpdateUserProfile);
+userRouter.get("/:id", loginProtect, userControllers.getUserAndProfileById);
 
 
 // --- ADMIN-ONLY ROUTES (Require Authentication & Authorization) ---
 
-userRouter.get("/",loginProtect,protectedToAdmin, userControlers.showAllUsers);
-userRouter.delete("/:id", loginProtect,protectedToAdmin, userControlers.deleteUserById);
+userRouter.get("/",loginProtect,protectedToAdmin, userControllers.showAllUsers);
+userRouter.delete("/:id", loginProtect,protectedToAdmin, userControllers.deleteUserById);
 
 export default userRouter;
