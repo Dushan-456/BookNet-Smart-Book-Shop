b99bfd94a335f2afb5ce2f2f6aa4e8b0c3446866
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-05 14:32:45 +0530

    Update authMiddleware.mjs

--- BookNet-Backend/src/middleware/authMiddleware.mjs
+++ BookNet-Backend/src/middleware/authMiddleware.mjs
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 import DB from "../db/db.mjs";
 
-
 // Middleware to check if the user is logged in--------------------------------------------------------------------------
 
 export const loginProtect = async (req, res, next) => {
@@ -44,20 +43,43 @@ export const loginProtect = async (req, res, next) => {
             .json({ message: "Not authorized, user not found" });
       }
 
-      next(); 
+      next();
    } catch (err) {
-      return   res.status(401).json({ message: 'Not authorized, token Invalid or Expied' });
-
+      return res
+         .status(401)
+         .json({ message: "Not authorized, token Invalid or Expied" });
    }
 };
 
 // Middleware to check if the user is an admin-------------------------------------------------------------
 
 export const protectedToAdmin = (req, res, next) => {
+   if (req.authUser && req.authUser.role === "ADMIN") {
+      next();
+   } else {
+      res.status(403).json({ message: "Not authorized as an admin" });
+   }
+};
 
-    if (req.authUser && req.authUser.role === 'ADMIN') {
-    next(); 
-  } else {
-    res.status(403).json({ message: 'Not authorized as an admin' });
-  }
+// This middleware ATTACHES the user if authenticated, but DOES NOT block guests-----------------------------------------------------------------
+
+export const attachUserIfAuthenticated = async (req, res, next) => {
+   const authHeader = req.headers.authorization;
+      try {
+         const token =
+            authHeader && authHeader.startsWith("Bearer ")
+               ? authHeader.split(" ")[1]
+               : req.cookies?.token;
+               
+         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+         req.authUser = await DB.user.findUnique({
+            where: { id: decoded.userId },
+            select: { id: true, email: true, username: true, role: true },
+         });
+      } catch (error) {
+         // Invalid token, just ignore and treat as guest
+         console.log("Invalid token found, proceeding as guest.");
+      }
+   
+   next();
 };
