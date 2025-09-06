c8f8748555576a116f6756c202f2a303688995bc
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-06 16:41:47 +0530

    Update server.mjs

--- BookNet-Backend/server.mjs
+++ BookNet-Backend/server.mjs
@@ -3,20 +3,22 @@ import express from 'express'
 import rootRouter from './src/Routers/index.mjs'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
-
+import path from 'path'; 
+import { fileURLToPath } from 'url'; 
 
 
 const server = express()
 const PORT = process.env.PORT || 5001
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.use(cors());
 
 server.use(express.json())
-server.use(cors());
 server.use(cookieParser()); 
 
-server.use(cookieParser())
-
-
+server.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 server.use("/api/v1",rootRouter)
 
