6a41cbac8536d3e4d09b07da0c2ba12b1b4f5762
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-06 16:42:16 +0530

    Update orderRoutes.mjs

--- BookNet-Backend/src/Routers/orderRoutes.mjs
+++ BookNet-Backend/src/Routers/orderRoutes.mjs
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginProtect } from "../middleware/authMiddleware.mjs";
+import { loginProtect, protectedToAdmin } from "../middleware/authMiddleware.mjs";
 import orderController from "../Controllers/orderController.mjs";
 import { shippingDetailsFieldsValidator } from "../middleware/validationMethods.mjs";
 
@@ -10,6 +10,12 @@ const orderRoutes = Router();
 // // --- PROTECTED ROUTES (Require Authentication) ---
 
 orderRoutes.post("/",loginProtect,shippingDetailsFieldsValidator(),orderController.createOrder);
-orderRoutes.get("/my-orders/:userId",loginProtect,orderController.getOrdersByUserId);
+orderRoutes.get("/my-orders/:userId",loginProtect,orderController.getAllOrdersByUserId);
+
+// --- ADMIN-ONLY ROUTES (Require Authentication & Authorization) ---
+
+orderRoutes.get("/",loginProtect,protectedToAdmin,orderController.getAllOrders);
+orderRoutes.get("/:id",loginProtect,protectedToAdmin,orderController.getOrderByOrderId);
+orderRoutes.put("/:id/status",loginProtect,protectedToAdmin,orderController.updateOrderStatus);
 
 export default orderRoutes;
