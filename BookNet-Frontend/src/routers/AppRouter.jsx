3328700889b52eadbd1c645a4447934fd1cc2196
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-07 00:06:22 +0530

    Update AppRouter.jsx

--- BookNet-Frontend/src/routers/AppRouter.jsx
+++ BookNet-Frontend/src/routers/AppRouter.jsx
@@ -1,66 +1,61 @@
 import {
-   BrowserRouter,
-   createBrowserRouter,
-   Route,
-   RouterProvider,
-   Routes,
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../pages/Home";
 import About from "../pages/About";
-import ProtectedRouter from "./ProtectedRouter";
 import Error404Page from "../Pages/Error404Page";
-
+import ProtectedRouter from "./ProtectedRouter"; 
+import LoginPage from "../Pages/LoginPage"; 
+import UserProfile from "../Pages/UserProfile"; 
 
 const router = createBrowserRouter([
-   {
-      path: "/",
-      element: <MainLayout />,
-      // errorElement: <Error404Page />,
-      children: [
-         {
-            index: true,
-            element: <Home />,
-         },
-         {
-            path: "about",
-            element: <About />,
-         },
-        //  {
-        //     path: "category/:CategoryID",
-        //     element: <CategoryItems />,
-        //  },
-        //  {
-        //     path: "product/:ProductID",
-        //     element: <SingleProductPage />,
-        //  },
-        //  {
-        //     path: "checkout",
-        //     element: <Checkout />,
-        //  },
-         {
-            element: <ProtectedRouter ProtectedRole="user" />,
-            children: [
-            //    {
-            //       path: "profile",
-            //       element: <UserProfile />,
-            //    },
-            ],
-         },
-         {
-            path: "*",
-            element: <Error404Page />,
-         },
-      ],
-   },
-   {
-      path: "/login",
-      element: <LoginPage />,
-   },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      // --- Public Routes -------------------------------------------------------------------------------------------------------------------
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+
+      // --- Protected Routes (User must be logged in) ---
+      {
+        element: <ProtectedRouter ProtectedRole="user" />, 
+        children: [
+          {
+            path: "profile", 
+            element: <UserProfile />,
+          },
+          // {
+          //   path: "checkout",
+          //   element: <Checkout />,
+          // },
+        ],
+      },
+
+      // --- Catch-all for 404 Not Found --------------------------------------------------------------------------------------------------------------------------------
+      {
+        path: "*",
+        element: <Error404Page />,
+      },
+    ],
+  },
+  // --- Routes without the MainLayout --------------------------------------------------------------------------------------------------------------------------------
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
 ]);
 
 const AppRouter = () => {
-   return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 };
 
 export default AppRouter;
