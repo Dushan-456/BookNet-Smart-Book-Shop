1d4f1d428d326283fd00c74b512b0c6c40f970fe
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-07 12:59:33 +0530

    Update HomePage.jsx

--- BookNet-Frontend/src/Pages/HomePage.jsx
+++ BookNet-Frontend/src/Pages/HomePage.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Loading from '../Components/Loading/Loading'
 
 const HomePage = () => {
+
+      const [loading,setLoading]= useState(true)
+
+
+    useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 1000) // 2 seconds fake loading
+
+    return () => clearTimeout(timer) // cleanup
+  }, [])
+
+
+    if(loading) return <Loading/>
+    
   return (
     <div>
       <h1>Home Page</h1>
