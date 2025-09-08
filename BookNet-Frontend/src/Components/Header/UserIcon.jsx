fdf8529fbd388dbe3db9fbd902c65347f2c7d8ed
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-08 16:49:41 +0530

    Update UserIcon.jsx

--- BookNet-Frontend/src/Components/Header/UserIcon.jsx
+++ BookNet-Frontend/src/Components/Header/UserIcon.jsx
@@ -3,96 +3,139 @@ import { useNavigate } from "react-router-dom";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import IconButton from "@mui/material/IconButton";
 import { Avatar } from "@mui/material";
-import LogoutIcon from "@mui/icons-material/Logout";
+import { useAuth } from '../../Context/AuthContext';
+
+
 
 const UserIcon = () => {
-   const [open, setOpen] = useState(false);
-   const [authenticate, setAuthenticate] = useState(true);
-   const dropdownRef = useRef(null);
-   const navigate = useNavigate();
 
-   useEffect(() => {
-      const handleClickOutside = (event) => {
-         if (
-            dropdownRef.current &&
-            !dropdownRef.current.contains(event.target)
-         ) {
-            setOpen(false);
-         }
-      };
+    const { isAuthenticated, user, logout } = useAuth();
 
-      document.addEventListener("mousedown", handleClickOutside);
-      return () =>
-         document.removeEventListener("mousedown", handleClickOutside);
-   }, []);
-   return (
-      <div className=" relative inline-block text-left" ref={dropdownRef}>
-         {authenticate ? (
-            <IconButton color="inherit" onClick={() => setOpen(!open)}>
-               <Avatar
-                  alt="Remy Sharp"
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjDGMp734S91sDuUFqL51_xRTXS15iiRoHew&s"
-                  sx={{ border: "2px solid #6841f3" }}
-               />
-            </IconButton>
-         ) : (
-            <IconButton color="inherit" onClick={() => setOpen(!open)}>
-               <AccountCircleIcon style={{ fontSize: "30px" }} />
-            </IconButton>
-         )}
+    const [open, setOpen] = useState(false);
+    const [authenticate, setAuthenticate] = useState(true);
+    const dropdownRef = useRef(null);
+    const navigate = useNavigate();
 
-         {open && (
-            <div className="custom-scroll absolute z-50 mt-2 w-max h-min   bg-white border border-gray-200 rounded-md shadow-lg">
-               {authenticate ? (
-                  <div className="p-5">
-                     <div className="flex items-center gap-2 mb-2 ">
-                        <Avatar
-                           alt="Remy Sharp"
-                           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjDGMp734S91sDuUFqL51_xRTXS15iiRoHew&s"
-                           sx={{ width: 50, height: 50 }}
-                        />{" "}
-                        <div>
-                           <p>Welcome Back</p>{" "}
-                           <h3 className="text-lg font-bold">Navodya Dushan</h3>
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target)
+            ) {
+                setOpen(false);
+            }
+        };
+
+        const handleLogout = () => {
+            logout();
+            navigate('/login');
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () =>
+            document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
+
+
+    function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
+}
+    return (
+        <div className=" relative inline-block text-left" ref={dropdownRef}>
+            {isAuthenticated ? (
+                <IconButton color="inherit" onClick={() => setOpen(!open)}>
+                    <Avatar
+                    {...stringAvatar(user.firstName + ' ' + user.lastName)}
+                        src={user?.image}
+                        sx={{ border: "2px solid #6841f3" }}
+                    />
+                </IconButton>
+            ) : (
+                <IconButton color="inherit" onClick={() => setOpen(!open)}>
+                    <Avatar alt="Kbk Kkh"  />
+                </IconButton>
+            )}
+
+            {open && (
+                <div className="custom-scroll absolute z-50 mt-2 w-max h-min   bg-white border border-gray-200 rounded-md shadow-lg">
+                    {isAuthenticated ? (
+                        <div className="p-5">
+                            <div className="flex items-center gap-2 mb-2 ">
+                                <Avatar
+                                    {...stringAvatar(user.firstName + ' ' + user.lastName)}
+                                    src={user?.image}
+                                    sx={{ width: 50, height: 50 }}
+                                />{" "}
+                                <div>
+                                    <p>Welcome Back</p>{" "}
+                                    <h3 className="text-lg font-bold">{user.firstName +' '+ user.lastName}</h3>
+                                </div>
+
+                            </div>
+                            <hr />
+                            <button className="flex items-center cursor-pointer w-full text-left px-4 py-2 hover:bg-gray-100">
+                                <span>My Address</span>
+                            </button>
+                            <button className="flex items-center cursor-pointer w-full text-left px-4 py-2 hover:bg-gray-100">
+                                <span>Settings</span>
+                            </button>
+                            <button className="flex items-center cursor-pointer w-full text-left px-4 py-2 hover:bg-gray-100">
+                                <span>Orders</span>
+                            </button>
+                            <button onClick={logout} className="flex justify-center cursor-pointer mt-2 items-center rounded-xl w-full bg-[#6841f3] text-white text-left px-4 py-2 hover:bg-[#3a305a]">
+                                <span className="text-center">Log Out</span>
+                            </button>
                         </div>
-                     
-                     </div>
-                     <hr />
-                     <button className="flex items-center cursor-pointer w-full text-left px-4 py-2 hover:bg-gray-100">
-                        <span>My Address</span>
-                     </button>
-                     <button className="flex items-center cursor-pointer w-full text-left px-4 py-2 hover:bg-gray-100">
-                        <span>Settings</span>
-                     </button>
-                     <button className="flex items-center cursor-pointer w-full text-left px-4 py-2 hover:bg-gray-100">
-                        <span>Orders</span>
-                     </button>
-                     <button className="flex justify-center cursor-pointer mt-2 items-center rounded-xl w-full bg-[#6841f3] text-white text-left px-4 py-2 hover:bg-[#3a305a]">
-                        <span className="text-center">Go to Profile</span>
-                     </button>
-                  </div>
-               ) : (
-                  <div className="p-5">
-                     <div className="flex items-center gap-2 mb-2 ">
-                        <p>Welcome to</p>{" "}
-                        <h3 className="text-lg font-bold">clickSHOP</h3>
-                     </div>
-                     <hr />
+                    ) : (
+                        <div className="p-5">
+                            <div className="flex items-center gap-2 mb-2 ">
+                                <p>Welcome to</p>{" "}
+                                <h3 className="text-lg font-bold">BookNet</h3>
+                            </div>
+                            <hr />
 
-                     <button
-                        onClick={() => navigate("/login")}
-                        className="cursor-pointer flex justify-center mt-5 rounded-xl items-center w-full bg-[#000000] text-white text-left px-4 py-2 hover:bg-[#3a305a]">
-                        <span className="text-center">Login</span>
-                     </button>
-                     <button className="flex cursor-pointer justify-center mt-5 rounded-xl items-center w-full bg-[#6841f3] text-white text-left px-4 py-2 hover:bg-[#3a305a]">
-                        <span className="text-center">Register</span>
-                     </button>
-                  </div>
-               )}
-            </div>
-         )}
-      </div>
-   );
+                            <button
+                                onClick={() => navigate("/login")}
+                                className="cursor-pointer flex justify-center mt-5 rounded-xl items-center w-full bg-[#000000] text-white text-left px-4 py-2 hover:bg-[#3a305a]">
+                                <span className="text-center">Login</span>
+                            </button>
+                            <button 
+                            onClick={() => navigate("/register")}
+                            className="flex cursor-pointer justify-center mt-5 rounded-xl items-center w-full bg-[#6841f3] text-white text-left px-4 py-2 hover:bg-[#3a305a]">
+                                <span className="text-center">Register</span>
+                            </button>
+                        </div>
+                    )}
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default UserIcon;
