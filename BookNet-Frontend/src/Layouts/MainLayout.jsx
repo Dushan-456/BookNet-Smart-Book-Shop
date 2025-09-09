970d4aa4cd8b10dec3d6baf1c2e2752f029178d5
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-09 15:34:43 +0530

    Update MainLayout.jsx

--- BookNet-Frontend/src/Layouts/MainLayout.jsx
+++ BookNet-Frontend/src/Layouts/MainLayout.jsx
@@ -1,17 +1,17 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../Components/Footer/footer";
 import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
 
 const MainLayout = () => {
-   return (
-      <div className=" w-full overflow-hidden min-h-screen" >
-         <Header />
-         <div className="container mx-auto">
-            <Outlet />
-         </div>
-         <Footer />
+  return (
+    <div className=" w-full overflow-hidden min-h-screen">
+      <Header />
+      <div className="container mx-auto">
+        <Outlet />
       </div>
-   );
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
