9110eada9756083c68ac5418a3e832fe2b3b57e7
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-09 15:26:02 +0530

    Update Footer.jsx

--- BookNet-Frontend/src/Components/Footer/Footer.jsx
+++ BookNet-Frontend/src/Components/Footer/Footer.jsx
@@ -3,11 +3,12 @@ const Footer = () => {
   return (
     <footer className="">
       <div className="md:flex md:container mx-auto">
-        <div className=" md:flex-3 bg-cyan-700"><h2>About Us
-        </h2></div>
-        <div className=" md:flex-3 bg-red-500"><h2>Services</h2></div>
-        <div className=" md:flex-3 bg-amber-300"><h2>Quick Links</h2></div>
-        <div className=" md:flex-3 bg-blue-700"><h2>fkbbjfbv</h2></div>
+        <div className=" md:flex-3 ">
+ 
+ </div>
+        <div className=" md:flex-3 "><h2 className="text-2xl text-center font-semibold">Services</h2></div>
+        <div className=" md:flex-3 "><h2 className="text-2xl text-center font-semibold">Quick Links</h2></div>
+        <div className=" md:flex-3 "><h2 className="text-2xl text-center font-semibold">fkbbjfbv</h2></div>
       </div>
       <div className=" bg-gray-800 text-white p-4   text-center">
         <p>&copy; {new Date().getFullYear()} BookNet. All Rights Reserved.</p>
