bcb68a8d54274867a7d87553fc307f6078570213
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-09 17:33:42 +0530

    Update FooterColumn1.jsx

--- BookNet-Frontend/src/Components/Footer/FooterColumn1.jsx
+++ BookNet-Frontend/src/Components/Footer/FooterColumn1.jsx
@@ -3,11 +3,16 @@ import logo from "../../assets/Images/BookNet-Logo-Small-without-BG.png";
 const FooterColumn1 = () => {
   return (
     <div className=" md:flex-3 flex flex-col items-center p-5 gap-5 ">
-      <img src={logo} alt="BookNet Logo" className="w-3xs" />
+      <div className="flex flex-col items-center">
+        <img src={logo} alt="BookNet Logo" className="w-3xs" />
+        <h3 className="text-center font-bold text-xl">
+          “Your One-Stop Online Bookshop & Service Center”
+        </h3>
+      </div>
       <p className="text-center md:text-justify">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt ducimus
-        obcaecati et quam pariatur eveniet nihil fugit quisquam labore possimus
-        vel facere ratione nesciunt ad architecto, nostrum nam quo perspiciatis.
+        Whether you’re a student looking for textbooks, a professional needing
+        quick document printing, or someone shopping for stationery, BookNet has
+        you covered. Simple, fast, and reliable
       </p>
     </div>
   );
