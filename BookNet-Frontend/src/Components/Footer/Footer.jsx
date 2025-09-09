116915becbb9f2fe144cf64e7a43af470b2cdbac
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-09 15:34:25 +0530

    Update Footer.jsx

--- BookNet-Frontend/src/Components/Footer/Footer.jsx
+++ BookNet-Frontend/src/Components/Footer/Footer.jsx
@@ -1,20 +1,27 @@
+// import logo from "../../assets/ImagesBookNet-Logo-Small-without-BG.png"
 
 const Footer = () => {
   return (
     <footer className="">
       <div className="md:flex md:container mx-auto">
         <div className=" md:flex-3 ">
- 
- </div>
-        <div className=" md:flex-3 "><h2 className="text-2xl text-center font-semibold">Services</h2></div>
-        <div className=" md:flex-3 "><h2 className="text-2xl text-center font-semibold">Quick Links</h2></div>
-        <div className=" md:flex-3 "><h2 className="text-2xl text-center font-semibold">fkbbjfbv</h2></div>
+          <img src="" alt="" />
+        </div>
+        <div className=" md:flex-3 ">
+          <h2 className="text-2xl text-center font-semibold">Services</h2>
+        </div>
+        <div className=" md:flex-3 ">
+          <h2 className="text-2xl text-center font-semibold">Quick Links</h2>
+        </div>
+        <div className=" md:flex-3 ">
+          <h2 className="text-2xl text-center font-semibold">fkbbjfbv</h2>
+        </div>
       </div>
       <div className=" bg-gray-800 text-white p-4   text-center">
         <p>&copy; {new Date().getFullYear()} BookNet. All Rights Reserved.</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
