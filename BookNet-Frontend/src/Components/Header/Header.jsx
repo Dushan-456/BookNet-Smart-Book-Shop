92a4b164470d8fa8756707f0472a78650f534af2
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-09 15:34:28 +0530

    Update Header.jsx

--- BookNet-Frontend/src/Components/Header/Header.jsx
+++ BookNet-Frontend/src/Components/Header/Header.jsx
@@ -24,94 +24,103 @@ import HeaderCategories from "./HeaderCategories";
 const pages = ["Products", "Pricing", "Blog"];
 
 function Header() {
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
 
-   const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-   };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-   const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-   };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-   return (
-      <div  style={{boxShadow: "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"}}>
-         <div  className="container   py-1 px-2 m-auto">
-            <div className="flex items-center">
-               <IconButton
-               
-                  sx={{ borderRadius: 3,display: { xs: "none", md: "flex" } }}
-                  onClick={() => navigate("/")}>
-                  <img src={logo} width={"300px"} alt="BookNet Logo" />
-               </IconButton>
+  return (
+    <div
+      style={{
+        boxShadow:
+          "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px",
+      }}
+    >
+      <div className="container   py-1 px-2 m-auto">
+        <div className="flex items-center">
+          <IconButton
+            sx={{ borderRadius: 3, display: { xs: "none", md: "flex" } }}
+            onClick={() => navigate("/")}
+          >
+            <img src={logo} width={"300px"} alt="BookNet Logo" />
+          </IconButton>
 
-               <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                  <SideDrawerMenu/>
-                 
-               </Box>
-               <Typography
-                  noWrap
-        
-                  sx={{
-                     mr: 2,
-                     display: { xs: "flex", md: "none" },
-                     flexGrow: 1,
-                  }}>
-                  <IconButton
-                     sx={{ borderRadius: 3 }}
-                     onClick={() => navigate("/")}>
-                     <img src={logo} width={"150px"} alt="BookNet Logo" className="" />
-                  </IconButton>
-               </Typography>
-               <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                  <div className="search">
-                     <input
-                        className="outline-none"
-                        type="text"
-                        placeholder="Search"
-                     />
-                     <IconButton color="black">
-                        <SearchIcon />
-                     </IconButton>
-                  </div>
-               </Box>
-
-               <div >
-                  <Tooltip title="Profile Details" >
-                     <UserIcon />
-                  </Tooltip>
-
-                  <Tooltip title="Notifications" sx={{display: { xs: "none", md: "inline" }}}>
-                     <IconButton
-                     sx={{width:50,height:50}}
-                        size="large"
-                        color="inherit">
-                        <Badge badgeContent={4} color="error">
-                           <NotificationsIcon />
-                        </Badge>
-                     </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Cart" >
-                     <IconButton
-                        onClick={() => navigate("/cart")}
-                        style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}
-                        size="large"
-                        color="inherit">
-                        <Badge badgeContent={4} color="error">
-                           <LocalMallIcon style={{ fontSize: "32px" }} />
-                        </Badge>
-                     </IconButton>
-                  </Tooltip>
-               </div>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <SideDrawerMenu />
+          </Box>
+          <Typography
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+            }}
+          >
+            <IconButton sx={{ borderRadius: 3 }} onClick={() => navigate("/")}>
+              <img src={logo} width={"150px"} alt="BookNet Logo" className="" />
+            </IconButton>
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <div className="search">
+              <input
+                className="outline-none"
+                type="text"
+                placeholder="Search"
+              />
+              <IconButton color="black">
+                <SearchIcon />
+              </IconButton>
             </div>
-         </div>
-         <div  className="container px-20 pb-2  m-auto hidden md:flex items-center justify-between" >
-            <HeaderCategories/>
-            <HeaderNavMenu />
-         </div>
+          </Box>
+
+          <div>
+            <Tooltip title="Profile Details">
+              <UserIcon />
+            </Tooltip>
+
+            <Tooltip
+              title="Notifications"
+              sx={{
+                display: { xs: "none", md: "inline" },
+              }}
+            >
+              <IconButton
+                sx={{ width: 50, height: 50, marginRight: "10px" }}
+                size="large"
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="error">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Cart">
+              <IconButton
+                onClick={() => navigate("/cart")}
+                style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}
+                size="large"
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="error">
+                  <LocalMallIcon style={{ fontSize: "32px" }} />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+          </div>
+        </div>
       </div>
-   );
+      <div className="container px-20 pb-2  m-auto hidden md:flex items-center justify-between">
+        <HeaderCategories />
+        <HeaderNavMenu />
+      </div>
+    </div>
+  );
 }
 export default Header;
