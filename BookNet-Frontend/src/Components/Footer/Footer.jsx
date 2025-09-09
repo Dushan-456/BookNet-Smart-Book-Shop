463cfd3724a2a0e1b7f9f20e7f46bdf3b2c718b9
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-09 15:51:01 +0530

    Update Footer.jsx

--- BookNet-Frontend/src/Components/Footer/Footer.jsx
+++ BookNet-Frontend/src/Components/Footer/Footer.jsx
@@ -1,19 +1,25 @@
-// import logo from "../../assets/ImagesBookNet-Logo-Small-without-BG.png"
+import logo from "../../assets/Images/BookNet-Logo-Small-without-BG.png";
 
 const Footer = () => {
   return (
-    <footer className="">
-      <div className="md:flex md:container mx-auto">
-        <div className=" md:flex-3 ">
-          <img src="" alt="" />
+    <footer className="bg-gray-300 pt-10">
+      <div className="md:flex md:container mx-auto pb-5">
+        <div className=" md:flex-3 flex flex-col items-center p-5 ">
+          <img src={logo} alt="BookNet Logo" className="w-3xs" />
+          <p className="text-center md:text-justify">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+            ducimus obcaecati et quam pariatur eveniet nihil fugit quisquam
+            labore possimus vel facere ratione nesciunt ad architecto, nostrum
+            nam quo perspiciatis.
+          </p>
         </div>
-        <div className=" md:flex-3 ">
+        <div className=" md:flex-3 flex flex-col items-center p-5 ">
           <h2 className="text-2xl text-center font-semibold">Services</h2>
         </div>
-        <div className=" md:flex-3 ">
+        <div className=" md:flex-3 flex flex-col items-center p-5 ">
           <h2 className="text-2xl text-center font-semibold">Quick Links</h2>
         </div>
-        <div className=" md:flex-3 ">
+        <div className=" md:flex-3 flex flex-col items-center p-5 ">
           <h2 className="text-2xl text-center font-semibold">fkbbjfbv</h2>
         </div>
       </div>
