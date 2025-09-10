3434d12a74ec521bedfee903cb063e0533a093c0
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-10 17:45:18 +0530

    Updated
    * Update UserIcon.jsx
    
    * Create CartPage.jsx
    
    * Update UserIcon.jsx
    
    * Update UserIcon.jsx
    
    * Update UserIcon.jsx
    
    * Update RegisterPage.jsx
    
    * Update Footer.jsx
    
    * Update Header.jsx
    
    * Create HeaderCategories.jsx
    
    * Update UserIcon.jsx
    
    * Update HeaderNavMenu.css
    
    * Create SideDrawerMenu.jsx
    
    * Update AuthContext.jsx
    
    * Update MainLayout.jsx
    
    * Create ContactPage.jsx
    
    * Update Error404Page.jsx
    
    * Update HomePage.jsx
    
    * Update AppRouter.jsx
    
    * Update Footer.jsx
    
    * Update Footer.jsx
    
    * Update Header.jsx
    
    * Update HeaderCategories.jsx
    
    * Update UserIcon.jsx
    
    * Update HeaderNavMenu.jsx
    
    * Update SideDrawerMenu.jsx
    
    * Update MainLayout.jsx
    
    * Update Footer.jsx
    
    * Update LoginPage.jsx
    
    * Update RegisterPage.jsx
    
    * Update Footer.jsx
    
    * Update LoginPage.jsx
    
    * Update Footer.jsx
    
    * Create FooterColumn1.jsx
    
    * Create FooterColumn2.jsx
    
    * Create FooterColumn3.jsx
    
    * Create FooterColumn4.jsx
    
    * Update Header.jsx
    
    * Create CheckoutPage.jsx
    
    * Update AppRouter.jsx
    
    * Update FooterColumn1.jsx
    
    * Update Header.jsx
    
    * Update CheckoutPage.jsx
    
    * Update AppRouter.jsx
    
    * Update Header.jsx
    
    * Update LoginPage.jsx
    
    * Update package-lock.json
    
    * Update package.json
    
    * Create CategoryItemsRound.jsx
    
    * Create Hero1.jsx
    
    * Create AllProducts.jsx
    
    * Create ProductItem.css
    
    * Create ProductItem.jsx
    
    * Create ProductsSlider.jsx
    
    * Update MainLayout.jsx
    
    * Update HomePage.jsx
    
    * Delete .gitattributes
    
    * updated
    
    * Create migration.sql
    
    * Update schema.prisma
    
    * Update seed.mjs
    
    * Update productControllers.mjs
    
    * Update AllProducts.jsx
    
    * Update ProductItem.jsx

---
+++ BookNet-Backend/prisma/migrations/20250910071158_add_product_attributes/migration.sql
@@ -0,0 +1,21 @@
+-- AlterTable
+ALTER TABLE "public"."Category" ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."Product" ADD COLUMN     "salePrice" DECIMAL(10,2);
+
+-- CreateTable
+CREATE TABLE "public"."ProductImage" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "altText" TEXT,
+    "isPrimary" BOOLEAN NOT NULL DEFAULT false,
+    "productId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductImage_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."ProductImage" ADD CONSTRAINT "ProductImage_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
