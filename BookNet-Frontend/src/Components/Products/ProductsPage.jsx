a6407ec344fde04a2533d94fb05f48ad132a5844
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-09-10 22:34:14 +0530

    Update ProductsPage.jsx

--- BookNet-Frontend/src/Components/Products/ProductsPage.jsx
+++ BookNet-Frontend/src/Components/Products/ProductsPage.jsx
@@ -109,7 +109,7 @@ const ProductsPage = () => {
                 </Typography>
             )}
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid  grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
                 {products.map((product) => (
                     <ProductItem
                         key={product.id}
