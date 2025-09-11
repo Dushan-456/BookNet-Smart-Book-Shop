27d331496ac515ee61f8a8fcd298b69d0ca96ebd
ShaliniAma <148237368+ShaliniAma@users.noreply.github.com>
2025-09-12 00:21:15 +0530

    Update index.mjs

--- BookNet-Backend/src/Routers/index.mjs
+++ BookNet-Backend/src/Routers/index.mjs
@@ -7,7 +7,6 @@ import orderRoutes from "./orderRoutes.mjs";
 import serviceRoutes from "./serviceRoutes.mjs";
 import deliveryRoutes from "./deliveryRoutes.mjs";
 
-
 const rootRouter = Router();
 
 rootRouter.get("/",(req,res)=> res.sendStatus(200));
